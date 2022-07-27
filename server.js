50239efdce396ca0471d00af119908f4293a2900
Stacy Oswald <stacy.oswald@stacys-mbp.home>
2022-07-27 11:22:41 +0100

    include style sheet and reference to server file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const app = express()
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
---
+++ test/server.test.js
@@ -0,0 +1,4 @@
+// How do I write this in jest?
+When I go to the landing page, I should see:
+    - The correct title of the page
+    - Some content on the page
