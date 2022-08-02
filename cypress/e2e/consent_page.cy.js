8f1e3e6a9a3088f51fb881379ad917776b257380
Andy <andy.ferrie@gmail.com>
2022-08-02 11:02:16 +0100

    update IDs

--- cypress/e2e/consent_page.cy.js
+++ cypress/e2e/consent_page.cy.js
@@ -8,40 +8,40 @@ describe('testing the consent page loads', () => {
     })
   
     it("it should have a heading", () => {
-      cy.get('#heading').contains('Storing your documents')
-      cy.get('#heading').should('have.class', 'govuk-heading-xl')
+      cy.get('#consent-heading').contains('Storing your documents')
+      cy.get('#consent-heading').should('have.class', 'govuk-heading-xl')
     })
   })
   
 describe('page contents', () => {
     it('has an explanation of what the portal is', () => {
-        cy.get('#lead-para').should('have.class', 'govuk-body-l')
-        cy.get('#lead-para').contains('In this section you can upload your identity documents.')
+        cy.get('#consent-lead-para').should('have.class', 'govuk-body-l')
+        cy.get('#consent-lead-para').contains('In this section you can upload your identity documents.')
     })
 
     it('has a link to the security and GDPR policy', () => {
-        cy.get('#security-gdpr-link').should('have.class', 'govuk-link')
-        cy.get('#security-gdpr-link').contains('here')
+        cy.get('#consent-security-gdpr-link').should('have.class', 'govuk-link')
+        cy.get('#consent-security-gdpr-link').contains('here')
     })
 
     it('has a link to required documents', () => {
-        cy.get('#required-documents-link').should('have.class', 'govuk-link')
-        cy.get('#required-documents-link').contains('here')
+        cy.get('#consent-required-documents-link').should('have.class', 'govuk-link')
+        cy.get('#consent-required-documents-link').contains('here')
     })
 
     it('has a continue button', () => {
-        cy.get('#btn-continue').should('have.class', 'govuk-button')
-        cy.get('#btn-continue').contains('Continue to upload')
+        cy.get('#consent-btn-continue').should('have.class', 'govuk-button')
+        cy.get('#consent-btn-continue').contains('Continue to upload')
     })
 
     it('has a skip upload button', () => {
-        cy.get('#btn-skip-upload').should('have.class', 'govuk-button')
-        cy.get('#btn-skip-upload').contains('Skip upload & sign out')
+        cy.get('#consent-btn-skip-upload').should('have.class', 'govuk-button')
+        cy.get('#consent-btn-skip-upload').contains('Skip upload & sign out')
     })
 
     it('checks consent checkboxes', () => {
-        cy.get('#checkbox-consent-stored').should('have.class', 'govuk-checkboxes__input')
-        cy.get('#checkbox-consent-used').should('have.class', 'govuk-checkboxes__input')
+        cy.get('#consent-checkbox-stored').should('have.class', 'govuk-checkboxes__input')
+        cy.get('#consent-checkbox-used').should('have.class', 'govuk-checkboxes__input')
         cy.get('fieldset input').check(['consent-stored', 'consent-used'])
     })
 })
